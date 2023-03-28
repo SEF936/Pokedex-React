94c62000b4ac115f9c852dbe0b1dcbc7bf8c69d7
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-03-28 15:48:31 +0200

    First props pokemon

--- pokedex-/src/components/PokemonCard.jsx
+++ pokedex-/src/components/PokemonCard.jsx
@@ -1,22 +1,12 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
  
- function PokemonCard(){
-    const pokemon = pokemonList[0];
+ function PokemonCard({pokemon}){
 
 
 return ( 
   
      <figure>
-       {pokemon.imgSrc === pokemonList[0].imgSrc ? <img src= {pokemonList[0].imgSrc} alt= {pokemonList[0].name}></img> : <p>???</p>}
+       {pokemon.imgSrc ? <img src= {pokemon.imgSrc} alt= {pokemon.name} ></img> : <p>???</p>}
        <figcaption>{pokemon.name}</figcaption>
      </figure> 
      
